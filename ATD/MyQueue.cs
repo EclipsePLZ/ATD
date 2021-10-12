27c4b3cbc78955c587c104150eed29dfb2c6ee30
Boris Pavlov <borzpavlov@gmail.com>
2021-10-12 23:55:35 +0500

    add new methods (Add, Peek)

--- ATD/MyQueue.cs
+++ ATD/MyQueue.cs
@@ -5,16 +5,42 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ATD {
-    unsafe class MyQueue<T> {
+    class MyQueue<T> {
 
         private T[] arr;
-
-        public MyQueue() {
-            
+        private int size;
+        private int front;
+        private int back;
+        private int numOfElem;
+        
+        public MyQueue(int size) {
+            this.size = size;
+            arr = new T[this.size];
+            front = 0;
+            back = -1;
+            numOfElem = 0;
         }
 
         public void Add(T newElem) {
+            if (back == size - 1) {
+                back = -1;
+                numOfElem = 0;
+            }
+            InsertElemIntoArray(newElem);
+        }
 
+        private void InsertElemIntoArray(T elem) {
+            back++;
+            arr[back] = elem;
+            numOfElem++;
+        }
+
+        public void Remove() {
+
+        }
+
+        public T Peek() {
+            return arr[front];
         }
     }
 }
--- ATD/Program.cs
+++ ATD/Program.cs
@@ -4,6 +4,7 @@ namespace ATD {
     class Program {
         static void Main(string[] args) {
             MyQueue<int> queue = new MyQueue<int> ();
+            
         }
     }
 }
